9e7739262fdc11b5dd8a388282e0f300784d96e5
Tye Blackie <blackie.tye@gmail.com>
2017-08-07 11:08:19 -0700

    Initial Commit

---
+++ ScrollView Image Galleries/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScrollView Image Galleries
+//
+//  Created by Tye Blackie on 2017-08-07.
+//  Copyright © 2017 Tye Blackie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
